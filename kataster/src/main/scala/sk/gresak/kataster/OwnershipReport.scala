ac7a9df4a828c89baddf075dbfa9e86118320aa8
Eduard Gresak <eduard.gresak@gmail.com>
2011-10-24 01:59:31 +0200

    Parsing owner 1

--- kataster/src/main/scala/sk/gresak/kataster/OwnershipReport.scala
+++ kataster/src/main/scala/sk/gresak/kataster/OwnershipReport.scala
@@ -2,12 +2,28 @@ package sk.gresak.kataster
 
 import java.text.SimpleDateFormat
 import java.sql.Timestamp
-import sk.gresak.util.PdfText
+import io.{Source, BufferedSource}
+import java.util.Date
 
 class OwnershipReport(path: String) {
-  val text: String = new PdfText(path).txt
+
+  //val text: String = new PdfText(path).txt
+  val source: BufferedSource = Source.fromFile("C:\\Users\\edo\\Documents\\Urbariat\\xxx.txt")
+  val text: String = source.mkString
+  //  val text: String = ""
+
+  /*
+    val source2: BufferedSource = Source.fromFile("C:\\Users\\edo\\Documents\\Urbariat\\zzz.txt")
+    val boxes: String = source2.mkString
+  */
+
   val actualizationRE = """Aktualizácia\skatastrálneho\sportálu:\s(\d\d\.\d\d\.\d\d\d\d)""".r
   val creationRE = """(?s)Dátum\svyhotovenia\s(\d\d\.\d\d\.\d\d\d\d).*?Čas\svyhotovenia:\s(\d\d\:\d\d\:\d\d)""".r
+  val headerRE = """(?s)Por.\sčíslo\sPriezvisko.*?vlastníka.*?\n""".r
+  val footerRE = """(?s)Informatívny\svýpis.*?\n""".r
+  val partSplitRE = """(?s)\nČASŤ\s[A-K]:.*?\n""".r
+  val ownerSplitRE = """(?s)\n(?=Účastník\správneho\svzťahu:.*\n\d+\s)""".r
+  val ownerBoxRE = """(?s)Účastník\správneho\svzťahu:\s(.*?)\n(\d+)\s(.*?)\s*(\d+\s*/\s*\d+).*?\n(.*?)\n?Dátum\snarodenia\s:\s(\d\d\.\d\d\.\d\d\d\d)(.*)""".r
 
   def storeReport() {
     val actualizedStr = actualizationRE findFirstMatchIn text match {
@@ -26,10 +42,33 @@ class OwnershipReport(path: String) {
     println("\nInserted id = " + id.get)
   }
 
+  def parseOwner(box: String): (String, Int, String, String, String, java.util.Date, String) =
+    box match {
+      case ownerBoxRE(participant, num, nameAddress, share, street, birthDateStr, entitlement) =>
+        (participant, Integer.parseInt(num), nameAddress, share, street, new SimpleDateFormat("dd.MM.yyyy") parse birthDateStr, entitlement)
+      case _ => throw new Exception("Owner box not parsed: " + box)
+    }
+
+  def processOwners(s: String) {
+    val ownerBoxArray = ownerSplitRE.split(s)
+    val owners: Array[(String, Int, String, String, String, Date, String)] = ownerBoxArray.map(parseOwner)
+    owners foreach (s => {
+      println(s._1)
+      println(s._2)
+      println(s._3)
+      println(s._4)
+      println(s._5)
+      println(s._6)
+      println(s._7)
+    })
+  }
+
   def process() {
-    storeReport()
-    val parts: Array[String] = text.split("""\nČASŤ\s[A-K]:.*""")
-    println(parts.size)
+    //storeReport()
+    val s1 = headerRE.replaceAllIn(text, "")
+    val s2 = footerRE.replaceAllIn(s1, "")
+    val parts: Array[String] = partSplitRE.split(s2)
+    processOwners(parts(2))
   }
 
 }
